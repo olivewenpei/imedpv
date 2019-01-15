6c28c18c2211b433efa50233a6494cee7ae3e46c
chloe <chloe.wang@g2-biopharma-services.com>
2019-01-15 01:20:27 -0500

    add product and workflow

---
+++ tests/TestCase/Controller/SdProductTypesControllerTest.php
@@ -0,0 +1,72 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\SdProductTypesController;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * App\Controller\SdProductTypesController Test Case
+ */
+class SdProductTypesControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.sd_product_types',
+        'app.sd_products'
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     */
+    public function testAdd()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEdit()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
