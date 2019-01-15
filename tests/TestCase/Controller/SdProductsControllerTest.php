a466835cd20f676cf3b594eaf1f9fae2c33bd45f
chloe <chloe.wang@g2-biopharma-services.com>
2019-01-15 01:20:26 -0500

    add product and workflow

--- tests/TestCase/Controller/SdProductsControllerTest.php
+++ tests/TestCase/Controller/SdProductsControllerTest.php
@@ -17,6 +17,8 @@ class SdProductsControllerTest extends IntegrationTestCase
      */
     public $fixtures = [
         'app.sd_products',
+        'app.sd_product_types',
+        'app.sd_sponsor_companies',
         'app.sd_product_workflows'
     ];
 
